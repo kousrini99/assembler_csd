df4c4468396cdeced038d0ee4bb4581674971e50
Koushik <kousrini99@gmail.com>
2020-11-30 12:27:51 +0530

    final

--- intergration.cpp
+++ intergration.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int main(int argc,char ** argv)
 {
-    char* input;
-    sprintf(input,"./Compiler %s",argv[1]);
-    system(input);
-    system("./VM");
-    system("./Assembler");
+    //char* input;
+    //sprintf(input,"./Compiler %s",argv[1]);
+    //system(input);
+    //system("./VM");
+    //system("./Assembler");
+    system("echo 'apple'");
+    cout<<"orange\n";
+    system("echo 'apple1'");
+    cout<<"orange1\n";
 }
